a3c3badc2d41a229920847c7107f57c93446588d
andrecouto <yogacouto@gmail.com>
2016-08-30 13:49:53 -0300

    ok

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.andrecouto.petteproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
