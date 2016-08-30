86979eea49daa986c2db0bc51d9f4466dba00b3d
andrecouto <yogacouto@gmail.com>
2016-08-30 13:57:09 -0300

    ok

--- MainActivity.java
+++ MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int id;
+    private int idx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
